b220f892c3e5397e6f4a2752c59d9245ccc6e577
CristianZen77 <96541038+CristianZen77@users.noreply.github.com>
2023-05-20 21:46:00 -0300

    Update codo.js
    Signed-off-by: CristianZen77 <96541038+CristianZen77@users.noreply.github.com>

--- js/codo.js
+++ js/codo.js
@@ -1,1 +1,61 @@
+Swal.fire({
 
+	title: "BURGER CODO A CODO",
+	text: "Te presentamos nuestro T.P.O.",
+	// html:
+	// icon:
+	// confirmButtonText:
+	// footer:
+	width: "50%",
+	// padding:
+	//background: "",
+	// grow:
+	// backdrop:
+	timer: "2500",
+	timerProgressBar: "true",
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton: "true",
+	confirmButtonColor: "yellow",
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	imageUrl: "imgages/logo.png",
+	imageWidth: "25%",
+	// imageHeight:
+	imageAlt: "codo a codo",
+})
